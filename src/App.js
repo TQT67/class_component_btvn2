1964a755b8d10d026b807d0ee10e05bd57dd9499
TQT67 <trangtrinh000000@gmail.com>
2023-02-13 15:10:35 +0700

    btvn2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      isValid: false,
+    };
+  }
+
+  handleUsernameChange = (e) => {
+    this.setState({ username: e.target.value });
+  };
+
+  handlePasswordChange = (e) => {
+    this.setState({ password: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { username, password } = this.state;
+    if (username === "ace" && password === "123456") {
+      this.setState({ isValid: true });
+    }
+  };
+
+  render() {
+    const { isValid } = this.state;
+    return (
+      <div className="login-form">
+        <h2>Login</h2>
+        {isValid && <p className="success-message">Đăng nhập thành công</p>}
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="formBasicUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter username"
+              onChange={this.handleUsernameChange}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={this.handlePasswordChange}
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Login;
